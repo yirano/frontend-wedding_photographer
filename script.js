b4bc6bffbfecaba7fcf763a27152253f955413d3
yirano <tiffany.arellano@outlook.com>
2018-10-04 11:02:14 -0700

    begin styling scrolled navbar

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const engagement = document.getElementById("serviceOne");
 
 engagement.addEventListener("mouseenter", function(){
-    
+
 });
+
+window.addEventListener('scroll', function(){
+    var navi = document.querySelector('.header');
+    navi.className += ' scrolledHeader';
+});
+

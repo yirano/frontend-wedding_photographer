62711eb85706549cad77fbbef65233f6e425f8fc
yirano <tiffany.arellano@outlook.com>
2018-10-04 18:12:13 -0700

    adjust position of nav elements on scroll

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ engagement.addEventListener("mouseenter", function(){
 window.addEventListener('scroll', function(){
     var navi = document.querySelector('.header');
 
-    if(this.window.scrollY >= 10) {
+    if(this.window.scrollY > 0) {
         navi.className += ' scrolledHeader';
     } else {
         console.log("reached top");

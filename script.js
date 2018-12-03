404b1955156bc81676e0cca932180cddf57c2047
yirano <tiffany.arellano@outlook.com>
2018-12-03 14:53:05 -0800

    initiate burger menu

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-const engagement = document.getElementById("serviceOne");
-const serviceBox = document.getElementsByClassName("serviceBox");
+document.addEventListener('DOMContentLoaded', function(event){
 
-engagement.addEventListener("mouseenter", function(){
+    const engagement = document.getElementById("serviceOne");
+    const serviceBox = document.getElementsByClassName("serviceBox");
+    const menuCheck = document.getElementById("menuCheck");
+    const burgerMenu = document.getElementById('burgerMenu');
 
-});
+    console.log("loaded");
 
-window.addEventListener('scroll', function(){
-    var navi = document.querySelector('.header');
+    engagement.addEventListener("mouseenter", function(){
 
-    if(this.window.scrollY > 0) {
-        navi.className += ' scrolledHeader';
-    } else {
-        console.log("reached top");
-        navi.classList.remove("scrolledHeader");
-    }
-});
-
-for(i = 0; i < serviceBox.length; i++) {
-    serviceBox[i].addEventListener('touchstart', function(){
-        this.classname= ".hover";
-        console.log("touchstart");
     });
 
-}
+    window.addEventListener('scroll', function(){
+        var navi = document.querySelector('.header');
+
+        if(this.window.scrollY > 0) {
+            navi.className += ' scrolledHeader';
+        } else {
+            console.log("reached top");
+            navi.classList.remove("scrolledHeader");
+        }
+    });
+
+    for(i = 0; i < serviceBox.length; i++) {
+        serviceBox[i].addEventListener('touchstart', function(){
+            this.classname= ".hover";
+            console.log("touchstart");
+        });
+    }
 
 
+    menuCheck.addEventListener('change', function(event){
 
+        if(menuCheck.checked) {
+            burgerMenu.style.display = 'block';
+            console.log('checked');
+        } else {
+            burgerMenu.style.display = 'none';
+            console.log('unchecked');
+        }
+    });
+        
 
+});
